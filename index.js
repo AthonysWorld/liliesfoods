29fcc9d4125c021acc61752fd6b76c4a5f8ed8d5
Adams Anthony Isioma <114524110+AthonysWorld@users.noreply.github.com>
2023-06-13 00:32:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// function toggleNavbar() {
+//     var navbar = document.getElementById("myNavbar");
+//     if (navbar.className === "navbar") {
+//       navbar.className += " responsive";
+//     } else {
+//       navbar.className = "navbar";
+//     }
+//   }
+
+
